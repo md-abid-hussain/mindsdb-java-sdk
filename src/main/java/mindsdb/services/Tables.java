e6e980c45f240542279b1efad598a79c360685c2
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-08 19:50:21 +0530

    fixed issue in Table.java

--- src/main/java/mindsdb/services/Tables.java
+++ src/main/java/mindsdb/services/Tables.java
@@ -1,24 +1,25 @@
 package mindsdb.services;
 
-import kong.unirest.core.GenericType;
-import kong.unirest.core.Unirest;
-import mindsdb.models.Model;
-import mindsdb.models.Table;
-import mindsdb.utils.Constants;
-
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+import kong.unirest.core.GenericType;
+import kong.unirest.core.Unirest;
+import mindsdb.models.Table;
+import mindsdb.utils.Constants;
+
 public class Tables {
 
-    public static List<Table> list(String databaseName){
+    public static List<Table> list(String databaseName) {
         AtomicReference<List<Table>> tableListAtomicRef = new AtomicReference<>();
         Unirest.get(Constants.LIST_MODEL_ENDPOINT)
-                .routeParam(Constants.PROJECT_NAME_ROUTE_PARAM, projectName)
-                .asObject(new GenericType<List<Table>>(){})
+                .routeParam(Constants.DATABASE_NAME_ROUTE_PARAM, databaseName)
+                .asObject(new GenericType<List<Table>>() {
+                })
                 .ifFailure(listHttpResponse -> {
                     if (listHttpResponse.getParsingError().isPresent()) {
-                        throw new RuntimeException("Not able to parse response. Error - " + listHttpResponse.getParsingError().get());
+                        throw new RuntimeException(
+                                "Not able to parse response. Error - " + listHttpResponse.getParsingError().get());
                     }
                 })
                 .ifSuccess(listHttpResponse -> {
