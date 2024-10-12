8c87442aedfc4b64ed8e22b101cf4bfcd6cfa1b4
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-12 23:46:03 +0530

    createed model and service for job

---
+++ src/main/java/mindsdb/client/Jobs.java
@@ -0,0 +1,145 @@
+package mindsdb.client;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import mindsdb.connectors.RestAPI;
+import mindsdb.models.Job;
+import mindsdb.models.Project;
+import mindsdb.utils.CreateJobCallback;
+import mindsdb.utils.DataFrame;
+
+public class Jobs {
+
+    private final Project project;
+    private final RestAPI api;
+
+    public Jobs(Project project, RestAPI api) {
+        this.project = project;
+        this.api = api;
+    }
+
+    /**
+     * List jobs in project
+     * 
+     * @param name job name
+     * @return list of Job objects
+     */
+    private List<Job> listJobs(String name) {
+        String astQuery = String.format("SELECT * FROM %s.jobs", project.getName());
+
+        if (name != null) {
+            astQuery += String.format(" WHERE name='%s';", name);
+        }
+
+        DataFrame response = api.sqlQuery(astQuery, this.project.getName());
+
+        return response.getRows().stream()
+                .map(row -> {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("query", row.get("QUERY") != null ? row.get("QUERY").toString() : null);
+                    data.put("start_at", row.get("START_AT") != null ? row.get("START_AT").toString() : null);
+                    data.put("end_at", row.get("END_AT") != null ? row.get("END_AT").toString() : null);
+                    data.put("schedule_str",
+                            row.get("SCHEDULE_STR") != null ? row.get("SCHEDULE_STR").toString() : null);
+                    Job job = new Job(this.project, row.get("NAME").toString(), data, null);
+                    return job;
+                })
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Show list of jobs in project
+     *
+     * @return list of Job objects
+     */
+    public List<Job> list() {
+        return listJobs(null);
+    }
+
+    /**
+     * Get job by name
+     *
+     * @param name job name
+     * @return Job object
+     */
+    public Job get(String name) {
+        return listJobs(name).stream()
+                .filter(job -> job.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Job doesn't exist"));
+    }
+
+    /**
+     * Create job in project
+     *
+     * @param name      name of the job
+     * @param queryStr  query string
+     * @param startAt   start time
+     * @param endAt     end time
+     * @param repeatStr repeat string
+     * @param repeatMin repeat minutes
+     * @return Job object
+     */
+    public Job create(String name, String queryStr, LocalDateTime startAt, LocalDateTime endAt, String repeatStr,
+            Integer repeatMin) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String startStr = (startAt != null) ? " START " + String.format("'%s'", startAt.format(formatter)) : null;
+        String endStr = (endAt != null) ? " END " + endAt.format(formatter) : null;
+        final String finalRepeatStr = (repeatMin != null) ? " EVERY " + repeatMin + " minutes"
+                : repeatStr != null ? " EVERY " + repeatStr : null;
+
+        CreateJobCallback createCallback = (query) -> {
+            StringBuilder astQuery = new StringBuilder("CREATE JOB " + name);
+            if (query != null) {
+                astQuery.append(" (").append(query).append(")");
+            }
+
+            if (startStr != null) {
+                astQuery.append(startStr);
+            }
+
+            if (endStr != null) {
+                astQuery.append(endStr);
+            }
+
+            if (finalRepeatStr != null) {
+                astQuery.append(finalRepeatStr);
+            }
+
+            // String astQuery = String.format("CREATE JOB %s WITH QUERY %s START %s END %s
+            // REPEAT %s",
+            // name, query, startStr, endStr, finalRepeatStr);
+            api.sqlQuery(astQuery.toString(), project.getName());
+        };
+
+        if (queryStr == null) {
+            Job job = new Job(project, name, createCallback);
+            return job;
+        } else {
+            createCallback.execute(queryStr);
+            List<Job> jobs = this.listJobs(name);
+
+            System.out.println("jobs " + jobs);
+            if (jobs.size() == 1) {
+                return jobs.get(0);
+            } else {
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Drop job from project
+     *
+     * @param name name of the job
+     */
+    public void drop(String name) {
+        String astQuery = String.format("DROP JOB %s", name);
+        api.sqlQuery(astQuery, project.getName());
+    }
+}
---
+++ src/main/java/mindsdb/models/Job.java
@@ -0,0 +1,122 @@
+package mindsdb.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import mindsdb.client.Query;
+import mindsdb.utils.CreateJobCallback;
+import mindsdb.utils.DataFrame;
+
+public class Job implements AutoCloseable {
+    public final Project project;
+    public final String name;
+    public String queryString;
+    public Map<String, String> data;
+    private List<String> queries;
+    private String startAt;
+    private String endAt;
+    private String nextRunAt;
+    private String scheduleStr;
+    private CreateJobCallback createCallback;
+
+    public Job(Project project, String name, Map<String, String> data, CreateJobCallback createCallback) {
+        this.project = project;
+        this.name = name;
+        this.data = data;
+        this.createCallback = createCallback;
+        this.queries = new ArrayList<>();
+
+        if (data != null) {
+            update(data);
+        }
+    }
+
+    public Job(Project project, String name, CreateJobCallback createCallback) {
+        this(project, name, null, createCallback);
+    }
+
+    private void update(Map<String, String> data) {
+        this.queryString = data.get("query");
+        this.startAt = data.get("start_at");
+        this.endAt = data.get("end_at");
+        this.nextRunAt = data.get("next_run_at");
+        this.scheduleStr = data.get("schedule_str");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(%s, query='%s')", this.getClass().getSimpleName(), name, queryString);
+    }
+
+    // public Job enter() {
+
+    // System.out.println(1);
+    // if (createCallback == null) {
+    // throw new IllegalStateException("The job is already created and can't be used
+    // to create context."
+    // + " To be able to use context: create job without 'query_str' parameter: ");
+    // }
+    // ContextManager.setSaving("job-" + name);
+    // return this;
+    // }
+
+    public void exit() {
+        // ContextManager.setSaving(null);
+        if (queries.isEmpty()) {
+            throw new IllegalStateException("No queries were added to job");
+        }
+
+        String queryStr = String.join("; ", queries);
+        // System.out.println("queryStr: " + queryStr);
+        createCallback.execute(queryStr);
+        refresh();
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.exit();
+    }
+
+    public void refresh() {
+        Job job = project.getJob(name);
+        update(job.data);
+    }
+
+    // public void addQuery(Object query) {
+    // if (query instanceof Query) {
+    // Query q = (Query) query;
+    // if (q.database != null && !q.database.equals(project.getName())) {
+    // throw new IllegalArgumentException("Wrong query database: " + q.database
+    // + ". You could try to use SQL string instead");
+    // }
+    // queries.add(q.sql);
+    // } else if (query instanceof String) {
+    // queries.add((String) query);
+    // } else {
+    // throw new IllegalArgumentException("Unable to use add this object as a query:
+    // " + query
+    // + ". Try to use SQL string instead");
+    // }
+    // }
+
+    public Job addQuery(Query query) {
+        if (query.database != null && !query.database.equals(project.getName())) {
+            throw new IllegalArgumentException("Wrong query database: " + query.database
+                    + ". You could try to use SQL string instead");
+        }
+        queries.add(query.sql);
+        return this;
+    }
+
+    public Job addQuery(String query) {
+        queries.add(query);
+        return this;
+    }
+
+    public DataFrame getHistory() {
+        String astQuery = String.format("SELECT * FROM log.jobs_history WHERE name = '%s'", name);
+        return project.api.sqlQuery(astQuery, project.getName());
+    }
+
+}
