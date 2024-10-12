feccc401e840bf3fdae040313fb0e5dd06756961
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-12 23:45:42 +0530

    created model and service for project

---
+++ src/main/java/mindsdb/client/Projects.java
@@ -0,0 +1,50 @@
+package mindsdb.client;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import mindsdb.connectors.RestAPI;
+import mindsdb.models.Project;
+import mindsdb.utils.DataFrame;
+
+public class Projects {
+    private final RestAPI api;
+    private final Server server;
+
+    public Projects(Server server, RestAPI api) {
+        this.api = api;
+        this.server = server;
+    }
+
+    private List<String> _listProjects() {
+        DataFrame response = api
+                .sqlQuery("SELECT NAME FROM information_schema.databases WHERE TYPE='project'");
+        return response.getColumn("NAME").stream()
+                .map(Object::toString)
+                .collect(Collectors.toList());
+    }
+
+    public List<Project> list() {
+        return _listProjects().stream()
+                .map(name -> new Project(name, api))
+                .collect(Collectors.toList());
+    }
+
+    public Project get(String name) {
+        if (!_listProjects().contains(name)) {
+            throw new IllegalArgumentException("Project doesn't exist");
+        }
+        return new Project(name, api);
+    }
+
+    public Project create(String name) {
+        String astQuery = String.format("CREATE DATABASE %s WITH ENGINE 'mindsdb'", name);
+        api.sqlQuery(astQuery);
+        return new Project(name, api);
+    }
+
+    public void drop(String name) {
+        String astQuery = String.format("DROP DATABASE %s", name);
+        api.sqlQuery(astQuery);
+    }
+}
---
+++ src/main/java/mindsdb/models/Project.java
@@ -0,0 +1,53 @@
+package mindsdb.models;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import mindsdb.client.Jobs;
+import mindsdb.client.Query;
+import mindsdb.connectors.RestAPI;
+
+public class Project {
+    private final String name;
+    public final RestAPI api;
+    public Jobs jobs;
+
+    public Project(String name, RestAPI api) {
+        this.name = name;
+        this.api = api;
+        this.jobs = new Jobs(this, api);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Query query(String sql) {
+        return new Query(api, sql, name);
+    }
+
+    public void dropModelVersion(String modelName, int version) {
+        String astQuery = String.format("DROP PREDICTOR %s.%d", modelName, version);
+        query(astQuery).fetch();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Project(%s)", name);
+    }
+
+    // Jobs
+    public List<Job> listJobs() {
+        return jobs.list();
+    }
+
+    public Job getJob(String name) {
+        return jobs.get(name);
+    }
+
+    public Job createJob(String name, String queryStr, LocalDateTime startAt, LocalDateTime endAt, String repeatStr,
+            Integer repeatMin) {
+        return jobs.create(name, queryStr, startAt, endAt, repeatStr, repeatMin);
+    }
+
+}
