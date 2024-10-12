eedc811fd9bbe89f66265f915c926f1ed39fc98d
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-13 00:11:38 +0530

    created database service and model

---
+++ src/main/java/mindsdb/client/Databases.java
@@ -0,0 +1,78 @@
+package mindsdb.client;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import mindsdb.connectors.RestAPI;
+import mindsdb.models.Database;
+import mindsdb.models.Handler;
+import mindsdb.utils.DataFrame;
+
+public class Databases {
+    private final RestAPI api;
+
+    public Databases(RestAPI api) {
+        this.api = api;
+    }
+
+    private List<Database> listDatabases() {
+        DataFrame response = api.sqlQuery("select NAME, ENGINE from information_schema.databases where TYPE='data'");
+
+        return response.getRows().stream().map(row -> {
+            String name = row.get("NAME").toString();
+            String engine = row.get("ENGINE").toString();
+            return new Database(this.api, name, engine);
+        }).collect(Collectors.toList());
+
+    }
+
+    public List<Database> list() {
+        return listDatabases();
+    }
+
+    public Database get(String name) {
+        return listDatabases().stream()
+                .filter(database -> database.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Database doesn't exist"));
+    }
+
+    public Database create(String name, Object engine, Map<String, String> connectionArgs) {
+
+        if (engine == null) {
+            throw new IllegalArgumentException("Engine is required");
+        }
+
+        if (engine instanceof Handler) {
+            engine = ((Handler) engine).getName();
+        }
+        StringBuilder astQuery = new StringBuilder(
+                "CREATE DATABASE " + name + " ENGINE = " + "'" + engine + "'");
+
+        if (connectionArgs != null) {
+            astQuery.append(", PARAMETERS = { ");
+            for (Map.Entry<String, String> entry : connectionArgs.entrySet()) {
+                astQuery.append("\\\"").append(entry.getKey().replace("\"", "\\\"")).append("\\\"")
+                        .append(": ")
+                        .append("\\\"").append(entry.getValue().replace("\"", "\\\"")).append("\\\"")
+                        .append(", ");
+            }
+            // Remove the trailing comma and space, and close the PARAMETERS object
+            astQuery.setLength(astQuery.length() - 2);
+            astQuery.append(" }");
+        } else {
+            astQuery.append(";");
+        }
+
+        astQuery = new StringBuilder(astQuery.substring(0, astQuery.length() - 2) + " };");
+        api.sqlQuery(astQuery.toString());
+        return new Database(this.api, name, engine.toString());
+    }
+
+    public void drop(String name) {
+        String astQuery = String.format("DROP DATABASE %s", name);
+        api.sqlQuery(astQuery);
+    }
+
+}
---
+++ src/main/java/mindsdb/models/Database.java
@@ -0,0 +1,27 @@
+package mindsdb.models;
+
+import mindsdb.client.Query;
+import mindsdb.connectors.RestAPI;
+
+public class Database {
+    // private Server server;
+    public String name;
+    private String engine;
+    public final RestAPI api;
+
+    public Database(RestAPI api, String name, String engine) {
+        // this.server = server;
+        this.name = name;
+        this.engine = engine;
+        this.api = api;
+    }
+
+    public Query query(String sql) {
+        return new Query(this.api, sql, this.name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(%s)", this.getClass().getSimpleName(), this.name);
+    }
+}
