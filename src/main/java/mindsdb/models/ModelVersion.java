aeb29959f7fc5d54902d8b45ba820b629c02be7e
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-16 23:22:17 +0530

    created ModelVersion class for ml model with version

---
+++ src/main/java/mindsdb/models/ModelVersion.java
@@ -0,0 +1,10 @@
+package mindsdb.models;
+
+import java.util.Map;
+
+public class ModelVersion extends Model {
+    public ModelVersion(Project project, Map<String, Object> data) {
+        super(project, data);
+    }
+
+}
