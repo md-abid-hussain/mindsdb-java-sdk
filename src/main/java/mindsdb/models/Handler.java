3cde1e900a8d106512d0c6cc3c5fe4db03c310b9
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-23 12:52:34 +0530

    added getter to handler model

--- src/main/java/mindsdb/models/Handler.java
+++ src/main/java/mindsdb/models/Handler.java
@@ -1,69 +1,95 @@
 package mindsdb.models;
 
-import kong.unirest.core.json.JSONObject;
-import lombok.Data;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
-@Data
+import lombok.Getter;
+
+@Getter
 public class Handler {
-    public String name;
-    public String title;
-    public String version;
-    public String description;
-    public JSONObject connectionArgs;
-    public boolean importSuccess;
-    public String importError;
+    private String name;
+    private String title;
+    private String version;
+    private String description;
+    private JsonObject connectionArgs;
+    private boolean importSuccess;
+    private String importError;
 
     // Constructor to handle dynamic parameters
-    public Handler(JSONObject kwargs) {
-        JSONObject normalizedKwargs = new JSONObject();
-        // JSONObject normalizedKwargs = kwargs;
+    public Handler(JsonObject kwargs) {
+        JsonObject normalizedKwargs = new JsonObject();
 
         // Normalize keys to lower case
         for (String key : kwargs.keySet()) {
             switch (key) {
                 case "name", "title", "version", "description" ->
-                    normalizedKwargs.put(key, (String) kwargs.get(key));
-                case "connection_args" ->
-                    normalizedKwargs.put(key,
-                            kwargs.get(key) != null && kwargs.getString(key).length() > 0 ? kwargs.get(key) : null);
+                    normalizedKwargs.addProperty(key, kwargs.get(key).getAsString());
+                case "connection_args" -> {
+                    JsonElement connectionArgsElement = kwargs.get(key);
+                    if (connectionArgsElement != null && connectionArgsElement.isJsonObject()) {
+                        normalizedKwargs.add(key, connectionArgsElement.getAsJsonObject());
+                    } else {
+                        normalizedKwargs.add(key, null);
+                    }
+                }
                 case "import_success", "import_error" -> {
-                    Object value = kwargs.get(key);
-                    boolean normalizedValue = value != null && !value.toString().equalsIgnoreCase("false");
-                    normalizedKwargs.put(key, normalizedValue);
+                    JsonElement value = kwargs.get(key);
+                    if (value != null && !value.isJsonNull()) {
+                        normalizedKwargs.add(key, value);
+                    } else {
+                        normalizedKwargs.add(key, null);
+                    }
                 }
             }
         }
 
         if (normalizedKwargs.has("name")) {
-            this.name = normalizedKwargs.getString("name");
+            this.name = normalizedKwargs.get("name").getAsString();
         }
         if (normalizedKwargs.has("title")) {
-            this.title = normalizedKwargs.getString("title");
+            this.title = normalizedKwargs.get("title").getAsString();
         }
         if (normalizedKwargs.has("version")) {
-            this.version = normalizedKwargs.getString("version");
+            this.version = normalizedKwargs.get("version").getAsString();
         }
         if (normalizedKwargs.has("description")) {
-            this.description = normalizedKwargs.getString("description");
+            this.description = normalizedKwargs.get("description").getAsString();
         }
         if (normalizedKwargs.has("connection_args")) {
-            Object connectionArgsValue = normalizedKwargs.get("connection_args");
-            if (connectionArgsValue != null) {
-                this.connectionArgs = new JSONObject(connectionArgsValue.toString());
+            JsonElement connectionArgsValue = normalizedKwargs.get("connection_args");
+            if (connectionArgsValue != null && connectionArgsValue.isJsonObject()) {
+                this.connectionArgs = connectionArgsValue.getAsJsonObject();
             } else {
                 this.connectionArgs = null;
             }
         }
         if (normalizedKwargs.has("import_success")) {
-            this.importSuccess = normalizedKwargs.getBoolean("import_success");
+            JsonElement importSuccessValue = normalizedKwargs.get("import_success");
+            if (importSuccessValue != null && !importSuccessValue.isJsonNull()) {
+                this.importSuccess = importSuccessValue.getAsBoolean();
+            } else {
+                this.importSuccess = false;
+            }
         }
         if (normalizedKwargs.has("import_error")) {
-            Object importErrorValue = normalizedKwargs.get("import_error");
-            if (importErrorValue != null) {
-                this.importError = importErrorValue.toString();
+            JsonElement importErrorValue = normalizedKwargs.get("import_error");
+            if (importErrorValue != null && !importErrorValue.isJsonNull()) {
+                this.importError = importErrorValue.getAsString();
             } else {
                 this.importError = null;
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(%s)", getClass().getSimpleName(), this.name);
+    }
+
+    public String describe() {
+        return String.format(
+                "%s(name=%s, title=%s, version=%s, description=%s, connectionArgs=%s, importSuccess=%s, importError=%s)",
+                getClass().getSimpleName(), this.name, this.title, this.version, this.description, this.connectionArgs,
+                this.importSuccess, this.importError);
     }
 }
