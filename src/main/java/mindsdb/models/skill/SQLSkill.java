8243b93eb2ae161300ff7892b65558e2ef01cf88
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-23 13:41:30 +0530

    updated sqlskill class

--- src/main/java/mindsdb/models/skill/SQLSkill.java
+++ src/main/java/mindsdb/models/skill/SQLSkill.java
@@ -2,6 +2,18 @@ package mindsdb.models.skill;
 
 import java.util.List;
 
+/**
+ * The SQLSkill class represents a specific type of skill that focuses on SQL
+ * tasks.
+ * It extends the Skill class and is initialized with a name, list of tables,
+ * database, and description.
+ * 
+ * @param name        The name of the SQL skill.
+ * @param tables      The list of tables associated with the SQL skill.
+ * @param database    The database associated with the SQL skill.
+ * @param description A brief description of the SQL skill.
+ * 
+ */
 public class SQLSkill extends Skill {
     public SQLSkill(String name, List<String> tables, String database, String description) {
         super(name, "sql", createParams(database, tables, description));
