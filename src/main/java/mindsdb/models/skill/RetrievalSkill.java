dba0a97e06b17cf5b39c3c67d5b1f6ef059bff46
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-10-23 12:59:59 +0530

    added comment to retrieval skilll java class

--- src/main/java/mindsdb/models/skill/RetrievalSkill.java
+++ src/main/java/mindsdb/models/skill/RetrievalSkill.java
@@ -1,7 +1,24 @@
 package mindsdb.models.skill;
 
+/**
+ * The RetrievalSkill class represents a specific type of skill that focuses on
+ * retrieval tasks.
+ * It extends the Skill class and is initialized with a name, knowledge base,
+ * and description.
+ * 
+ * @param name          The name of the retrieval skill.
+ * @param knoweldgeBase The knowledge base associated with the retrieval skill.
+ * @param description   A brief description of the retrieval skill.
+ */
 public class RetrievalSkill extends Skill {
 
+    /**
+     * Constructs a new RetrievalSkill instance.
+     *
+     * @param name          The name of the skill.
+     * @param knoweldgeBase The knowledge base associated with the skill.
+     * @param description   A description of the skill.
+     */
     public RetrievalSkill(String name, String knoweldgeBase, String description) {
         super(name, "retrieval", createParams(knoweldgeBase, null, description));
     }
